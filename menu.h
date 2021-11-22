27ce13c6b2302be4e6d13770c5554e02da08e14e
Anders Franzen <anders.franzen@yahoo.se>
2021-11-23 00:28:11 +0100

    readded menu.c and menu.h

---
+++ menu.h
@@ -0,0 +1,16 @@
+
+extern struct Panel *RawData;
+int init_RawData();
+int update_RawData(int fd, char *indata);
+
+extern struct Panel *FlagData;
+int init_FlagData();
+int update_FlagData(int fd, struct ECM_Regs *eregs);
+
+extern struct Panel *SensorData;
+int init_SensorData();
+int update_SensorData(int fd, struct ECM_Regs *eregs);
+
+extern struct Panel *ErrorData;
+int init_ErrorData();
+int update_ErrorData(int fd, struct ECM_Regs *eregs);
